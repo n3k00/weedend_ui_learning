7663dd61687ca02c9b742eca15297b906913c412
n3k00 <shinthantaung@ucspin.edu.mm>
2023-11-12 13:01:19 +0630

    start sign up page ui

--- profiles/lib/main.dart
+++ profiles/lib/main.dart
@@ -2,16 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:profiles/views/profile_one.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Profile UI',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
---
+++ sign_up_page_ui_design/README.md
@@ -0,0 +1,9 @@
+# sign_up_page_ui_design
+
+A new Flutter project.
+
+## Getting Started
+
+
+[Sing up page UI Design Resource](https://dribbble.com/shots/23030348-Sign-up-page-UI-design), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
